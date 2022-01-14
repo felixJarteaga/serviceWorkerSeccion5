462db1526267b3da32df789430a104189d5ce5de
Felix Arteaga <farteaga21791@gmail.com>
2022-01-14 18:07:20 +0100

    comenzando sección 5 SW

---
+++ sw.js
@@ -0,0 +1,21 @@
+
+// Ciclo de vida del SW
+
+self.addEventListener('install', event =>{
+
+  // Descargar los assets
+// Creamos un cahe
+
+  console.log('SW: Instalñando SW');
+
+});
+
+// Cunado el SW toma el control de la App
+
+self.addEventListener('activate', event =>{
+
+  // Borrar cache viejo
+  console.log('SW: Activo y listo para controlar la App');
+
+});
+
