acfa45b7593ea5686d54c716ebe8483939360c48
Felix Arteaga <farteaga21791@gmail.com>
2022-01-14 19:18:30 +0100

    fin sección 5

--- sw.js
+++ sw.js
@@ -8,14 +8,61 @@ self.addEventListener('install', event =>{
 
   console.log('SW: Instalñando SW');
 
+  const instalacion = new Promise( (resolve, reject) =>{
+
+    setTimeout(() => {
+      console.log('SW: Instalaciones terminadas');
+      self.skipWaiting();
+      resolve();
+    }, 1);
+
+  } );
+
+  // Para esperar hasta ( en nuestro caso hasta que se resulelva nuestra promesa )
+  event.waitUntil( instalacion );
+
+  // Para que se active el nuevo SW directamente
+  // self.skipWaiting();
+
 });
 
-// Cunado el SW toma el control de la App
+// Cuando el SW toma el control de la App
 
 self.addEventListener('activate', event =>{
 
   // Borrar cache viejo
-  console.log('SW: Activo y listo para controlar la App');
+  console.log('SW2: Activo y listo para controlar la App');
+
+});
+
+// FETCH: Manejo de peticione HTTP
+self.addEventListener('fetch', event =>{
+
+  //Aplicar estrategias del Cache 
+
+  // console.log('SW: ', event.request.url);
+
+  // if ( event.request.url.includes( 'https://reqres.in/' ) ) {
+  //   const respuesta = new Response(`{ok:false, mensaje: 'jajaja'}`);
+  //   event.respondWith( respuesta );
+  // }
+
+});
+
+// SYNC: Recuperamos la conexión a internet
+self.addEventListener('sync', event =>{
+
+  console.log('Recuperamos conexión');
+  console.log(event);
+  console.log(event.tag);
+
+});
+
+// PUSH: Manejar las push notifications
+self.addEventListener('push', event =>{
+
+  console.log('Notificación recivida');
+
 
 });
 
